71d80a77c7cca127ae6c6c7657a51066ffc8490d
ezzik1 <exstreamm96@gmail.com>
2022-07-14 16:43:44 +0300

    init: start task

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArtistController } from './artist/artist.controller';
+import { TrackController } from './track/track.controller';
+import { AlbumController } from './album/album.controller';
+import { FavoritesController } from './favorites/favorites.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UserModule],
+  controllers: [
+    AppController,
+    ArtistController,
+    TrackController,
+    AlbumController,
+    FavoritesController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
